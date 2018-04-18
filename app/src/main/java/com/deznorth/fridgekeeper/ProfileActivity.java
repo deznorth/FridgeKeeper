5ce23021d8aa012b7924959d46b63df096dd38cd
David Rojas <davo2211.5@gmail.com>
2018-04-18 14:11:48 -0400

    Initial commit

---
+++ app/src/main/java/com/deznorth/fridgekeeper/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.deznorth.fridgekeeper;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ProfileActivity extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        return rootView;
+    }
+}
